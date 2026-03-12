a69d1aa6a9f93a78fe4a5bd4162ee3477cf6feca
marcneil <marcnz0426@gmail.com>
2026-03-12 22:36:00 +0800

    added new JS logic for application-container and made a table for the job application list

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const positionInput = document.getElementById("position")
 const dateInput = document.getElementById("date")
 const statusInput = document.getElementById("status")
 const applicationForm = document.getElementById("add-application-form")
+const applicationContainer = document.getElementById("application-container");
+const noApplicationsAddedYet = document.getElementById("empty-state")
 
 applicationForm.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -21,6 +23,40 @@ applicationForm.addEventListener("submit", function(event) {
 
     // clear form and update the list on sidebar form
     applicationForm.reset();
+        renderApplication();
+    });
 
-    console.log(jobApplicationList)
-});
+function renderApplication() {
+    applicationContainer.innerHTML = "";
+
+    // .forEach needs to be inside the function renderApplication
+
+jobApplicationList.forEach(function(application) {
+    // HTML string using backticks and inject the data
+       const applicationCardHTML =`
+            <table class="card">
+                <caption>Job Applications</caption>
+                <thead>
+                    <tr>
+                        <th>Company</th>
+                        <th>Position</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>${application.companyName}</th>
+                        <td>${application.position}</td>
+                        <td>${application.applicationDate}</td>
+                        <td>${application.status}</td>
+                    </tr>
+                </tbody>
+            </table>
+       `;
+       // Append (add) this new HTML string into our container
+       applicationContainer.innerHTML += applicationCardHTML;
+
+        noApplicationsAddedYet.style.display = "none";
+    });
+}
